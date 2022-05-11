ed3c668b43cc1b26539f9aaa3d6438a7ba55953e
Jon Kemm <83780524+jonkemm@users.noreply.github.com>
2022-05-11 20:14:34 +0100

    greg

--- public/careers.php
+++ public/careers.php
@@ -7,10 +7,11 @@ include('./php/nav.php');
 ?>
 <h1><a name="#careers"><?=$title?></a></h1>
     <div class="row flex">
-        <div class="w-50" id="careers-bg"></div>
         <div class="w-50">
-        <p>At Viksta Care Ltd we provide high standards of care to clients in their own homes, we really feel we stand out from the rest and work hard to provide quality care for our customers. Our care workers are as important as the people that we care for.</li>
-        <p>We are looking for care workers / care assistants at our Brighton Branch. We have full and part time work available with flexible working patterns to suit you such as weekends, evenings and early mornings.</li>
+            <p>At Viksta Care Ltd we provide high standards of care to clients in their own homes, we really feel we stand out from the rest and work hard to provide quality care for our customers. Our care workers are as important as the people that we care for.</p>
+            <p>We are looking for care workers / care assistants at our Brighton Branch. We have full and part time work available with flexible working patterns to suit you such as weekends, evenings and early mornings.</p>
+        </div>
+        <div class="w-50" id="careers-bg"></div>
     </div>  
     <div class="row flex details">
         <div class="w-50">
@@ -39,8 +40,6 @@ include('./php/nav.php');
                 <li>Supporting our clients with their personal care, getting dressed, washing, bathing and toileting</li>
                 <li>Accompanying our clients to attend their medical appointments, day centres and social activities</li></li>
             </ul>
-        </div>
-        <div class="w-50">
             <h2>What is needed:</h2>
             <ul>
                 <li>Drivers licence and a vehicle</li>
@@ -49,9 +48,17 @@ include('./php/nav.php');
                 <li>Willingness to learn</li>
             </ul>
         </div>
-        <div class="w-50">
+        <div class="row">
             <p>In order to ensure the safety of staff and the vulnerable people we support during the Covid-19 pandemic, all staff will be required to comply with company, PHE and government recommendations including weekly testing.
             <p>We are always looking for talented people to join our team. If you are interested in working with us, please fill out the form below and we will get back to you as soon as possible.</p>
+        </div>
+        <div class="row">
+            <div class="w-25"></div>
+            <div class="w-50">
+                <a href="contact">
+                    <button class="btn btn-warning w-100" id="careers-form-btn">Join us</button>
+                </a>
+            </div>
         </div>
     </div>
 </div>
